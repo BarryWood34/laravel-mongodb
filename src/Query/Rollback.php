da3267938670531f573576449011fa1f724ea94e
Joy Prakash Sharma <droidlabour@gmail.com>
2023-03-04 06:32:09 +0530

    Log changes to rollback.log while testing
    Why:
    *   rollback.log will be used to revert any changes to the test database
    so it always stays fresh after every test case run.

---
+++ src/Query/Rollback.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Jenssegers\Mongodb\Query;
+
+class Rollback
+{
+    public function __construct(private string $collection, private string $operation, private array $ids)
+    {
+        $oplogs = $this->read();
+
+        $oplogs = array_merge_recursive($oplogs, $this->prepareNewOplog());
+
+        $this->write($oplogs);
+    }
+
+    private function read(string $filename = 'rollback.json'): array
+    {
+        if ( file_exists(storage_path($filename)) && $content = file_get_contents(storage_path($filename)) )
+        {
+            return json_decode($content, true) ?? [];
+        }
+        else
+        {
+            return [];
+        }
+    }
+
+    private function write(array $content, string $filename = 'rollback.json')
+    {
+        file_put_contents(storage_path($filename), json_encode($content, JSON_PRETTY_PRINT));
+    }
+
+    private function prepareNewOplog(): array
+    {
+        if ( $this->operation === 'update' )
+        {
+            $result['update'][] = [ $this->collection, $this->ids ];
+        }
+        elseif ( $this->operation === 'create' )
+        {
+            $result['create'][] = [ $this->collection, $this->ids ];
+        }
+
+        return $result ?? [];
+    }
+}
